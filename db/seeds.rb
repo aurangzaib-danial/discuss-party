5fab04e2f76792ec0f65f395ca461809a95c94e5
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-10 02:10:57 +0500

    setup some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 User.create!(email: 'aurangzaib.danial@gmail.com', password: '5w9u6jGs#iZv')
 
-%w[Game Book].each do |category|
+tags = Tag.create!([{name: 'games'}, {name: 'reading'}])
+other_tag = Tag.create(name: 'interesting')
+%w[Game Book].each.with_index do |category, index|
   user = User.create!(email: Faker::Internet.email, password: Devise.friendly_token[0..20])
   3.times do
     title = Faker.const_get(category).unique.title
-    Topic.create!(title: title, user: user)
+    topic = Topic.create!(title: title, user: user)
+    TopicTag.create!(topic: topic, tag: tags[index])
+    TopicTag.create!(topic: topic, tag: other_tag)
   end
 end
