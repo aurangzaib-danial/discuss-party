f18631d728c16fb15b3e9eaef99b69e0eb7793b6
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-10 03:27:02 +0500

    defined status as an enum in topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,5 @@ class Topic < ApplicationRecord
   belongs_to :user
   has_many :topic_tags
   has_many :tags, through: :topic_tags
+  enum status: { public: 0, private: 1 }, _prefix: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Topic, type: :model do
   it { should belong_to(:user)}
   it { should have_many(:topic_tags) }
   it { should have_many(:tags).through(:topic_tags) }
+  it do 
+    should define_enum_for(:status).
+      with_values(public: 0, private: 1).
+      with_prefix(:status)
+  end
 end
