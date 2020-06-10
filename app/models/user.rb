af535c2500389f6c52e5b57e860a7930b822c9e1
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-11 00:08:17 +0500

    wrote validations for name attribute in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :topics
+  validates_presence_of :name
+  validates_format_of :name, without: /[^A-Za-z\s]/
+  validates_length_of :name, maximum: 50
+  strip_attributes only: :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many :topics }
   end
+  it { should have_db_column :name }
+  it { should validate_presence_of :name }
+  it { should_not allow_value('Sunny123#$').for(:name) }
+  it { should validate_length_of(:name).is_at_most(50)}
+  it { should strip_attribute(:name) }
 end
