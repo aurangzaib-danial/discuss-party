3dbeb78ff1f06a97202c1d601699fea2e6064325
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-10 13:32:30 +0500

    completed topic validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,7 @@ class Topic < ApplicationRecord
   has_many :tags, through: :topic_tags
   enum visibility: { public: 0, private: 1 }, _prefix: true
   strip_attributes only: :title
+  validates_length_of :title, in: 5..70
+  validates_format_of :title, without: /[^a-zA-Z\d\s]/
+  validates_length_of :description, minimum: 20
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -14,8 +14,14 @@ RSpec.describe Topic, type: :model do
   end
 
   describe 'validations' do
-    # title between 5 - 70
-    # only numbers, letters and spaces in the middle, strip trailing spaces
     it { should strip_attribute :title }
+    it { should validate_length_of(:title).is_at_least(5).is_at_most(70)}
+    
+    describe 'title can only have letters, numbers and spaces' do
+      it { should allow_value('The trees around us are our saviours 123').for(:title)}
+      it { should_not allow_value('##This is a title!!! With bad characters!').for(:title)}
+    end
+    
+    it { should validate_length_of(:description).is_at_least(20)}
   end 
 end
