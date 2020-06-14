4b15e1bbf16a52724b3adaffae74960eeca02ae7
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-14 12:08:43 +0500

    switch to requests spec from controllers spec

--- spec/requests/topics_request_spec.rb
+++ spec/requests/topics_request_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Topics", type: :request do
+  subject { response }
+  describe '/topics/new' do
+    context 'not signed in' do
+      before { get new_topic_path }
+      it { should be_redirect }
+    end
 
+    context 'signed in' do
+      before do
+        user = create(:user)
+        sign_in user
+        get new_topic_path
+      end
+      it { should be_successful }
+    end
+  end
 end
