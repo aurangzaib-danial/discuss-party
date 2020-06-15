6e81d8e0ba82fcabfce5c8240b10ae116fa1c633
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-15 12:05:57 +0500

    comment feature is complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_topic
   def create
     @comment = Comment.new(comment_params)
--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-
-end
+  describe 'POST /topics/:id/comments' do
+    it 'cannot launch a post request if not signed in' do
+      topic = create(:topic)
+      post topic_comments_path(topic), params: { comment: { content: 'One'} }
+      expect(response).to be_redirect
+    end
+  end
+end 
