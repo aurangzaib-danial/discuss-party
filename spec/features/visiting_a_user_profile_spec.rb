c969cf0b5b5d2fadc6a37c40384f8b7f08ec44d7
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-29 23:47:47 +0500

    working on user's profile

--- spec/features/visiting_a_user_profile_spec.rb
+++ spec/features/visiting_a_user_profile_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.feature 'Visiting a user profile', type: 'feature' do
-  let(:user) {create(:user)}
+  subject(:user) {create(:user)}
+  let(:topics) { subject.topics }
+
+  def visit_profile
+    visit user_slug_path(subject.id, subject.slug)
+  end
+
+  def profile_with_topics
+    2.times { create(:topic, user: subject) }
+    visit_profile
+  end
 
   scenario 'visiting a user profile, shows user name' do
-    visit user_slug_path(user.id, user.slug)
-    expect(page.body).to have_content(user.name)
+    visit_profile
+    expect(page).to have_content(subject.name)
   end
- 
+
+  scenario 'shows no content message if no topic is available' do
+    visit_profile
+    message = "#{subject.name.titlecase} has not created a topic yet."
+    expect(page).to have_content(message)
+  end
+
 end
