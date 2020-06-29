ed070dce54af7c69cc0edba816e6671a043e4dc5
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-29 19:07:16 +0500

    implemented sorting feature on tag view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,22 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: :show
+  
   def show
+    @topics = @tag.topics.for_list_view
+
+    if params[:view] == 'oldest'
+      @topics = @topics.oldest
+    elsif params[:view] == 'popular'
+      @topics = @topics.popular
+    else
+      @topics = @topics.latest
+    end
+
+    Topic.find_votes_for(@topics, current_user)
   end
 
   private
   def set_tag
     @tag = Tag.find_by_slug(params[:slug])
-    if @tag.nil?
-      raise ActiveRecord::RecordNotFound, 'Cant do it'
-    end
   end
 end
--- spec/features/visiting_a_tag_view.rb
+++ spec/features/visiting_a_tag_view.rb
@@ -2,24 +2,61 @@ require 'rails_helper'
 
 RSpec.feature 'Visiting a tag view', type: 'feature' do
   subject { create(:tag) }
+  let(:topics) { subject.topics }
+
+  def visit_tag
+    visit tag_slug_path(subject.slug)
+  end
+
+  def visit_tag_with_topics
+    another_tag = build(:tag)
+    2.times do
+      create(:topic, topic_tags: [], tags: [subject, another_tag])
+    end
+    visit_tag
+  end
 
   scenario 'shows the tag name' do
-    visit tag_slug_path(subject.slug)
+    visit_tag
     expect(page).to have_content(subject.name)
   end
 
   scenario 'shows no content message if no topic is available' do
-    visit tag_slug_path(subject.slug)
+    visit_tag
     message = "No topic available for #{subject.name.capitalize}, be the first to create one."
     expect(page).to have_content(message)
   end
 
-  scenario 'lists all the available topics' do
-    another_tag = build(:tag)
-    topics = 2.times.collect do
-      topic = create(:topic, topic_tags: [], tags: [subject, another_tag])
-    end
-    visit tag_slug_path(subject.slug)
+  scenario 'lists topics of the tag only' do
+    unreleated_topic = create(:topic)
+    visit_tag_with_topics
     run_expectations_for_topics(topics)
+    expect(page).not_to have_content(unreleated_topic.title)
   end
+
+  scenario 'lists latest topics by default' do
+    visit_tag_with_topics
+    expect(get_topic_titles).to eq([topics.second.title, topics.first.title])
+  end
+
+  scenario 'clicking oldest, lists topics by oldest' do
+    visit_tag_with_topics
+    click_link :oldest
+    expect(current_path).to have_content(subject.name)
+    expect(get_topic_titles).to eq([topics.first.title, topics.second.title])
+  end
+
+  scenario 'clicking popular, lists topics by popularity' do
+    visit_tag_with_topics
+
+    topics << create(:topic)
+    topics.each.with_index(1) do |topic, index|
+      topic.vote(topic.user, :like) if index == 2 || index == 3
+      topic.vote(create(:user), :like) if index == 2
+    end
+
+    click_link :popular
+    expect(get_topic_titles).to eq([topics.second.title, topics.third.title, topics.first.title])
+  end
+
 end
