863edf59d4feec1a6d9e75712afa8dd725d08af9
Aurangzaib <aurangzaib.danial@gmail.com>
2020-06-29 23:38:01 +0500

    wrote user_no_topic_message helper for displaying correct error message for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,16 @@ module UsersHelper
   def short_user_name(user)
     user.name.length <= 10 ? user.name : (user.name[0..9] + '...')
   end
+
+  def user_no_topic_message(guest, current)
+    if current == guest
+      message = 'You have not created a topic yet.'
+      path = new_topic_path
+    else
+      message = "#{guest.name.titlecase} has not created a topic yet."
+      path = root_path
+    end
+
+    no_content_message(message, path)
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe UsersHelper, type: :helper do
       expect(helper.short_user_name(user)[-3..-1]).not_to eq('...')
     end
   end
+
+  describe 'user_no_topic_message' do
+    let(:current_user) {create(:user)}
+    let(:guest) {create(:user)}
+    let(:third_person) {create(:user)}
+
+    it 'returns message for current user if they are visiting their own profile' do
+      expect(helper.user_no_topic_message(current_user, current_user)).to include('You have not created a topic yet.')
+    end
+
+    it 'returns message for user when they are visiting some one else\'s profile' do
+      expect(helper.user_no_topic_message(third_person, current_user)).to include("#{third_person.name.titlecase} has not created a topic yet.")
+    end
+  end
 end
