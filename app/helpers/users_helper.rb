4b4a48b58c388ee02c41947c727929bf488f9a78
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-03 02:45:25 +0500

    decreased user's name characters in navbar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def short_user_name(user)
-    user.name.length <= 20 ? user.name : (user.name[0..19] + '...')
+    user.name.length <= 10 ? user.name : (user.name[0..9] + '...')
   end
 
   def user_no_topic_message(guest, current)
