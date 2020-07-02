cd05b84907bcf8b5d497cb3c47e16ff9d3557ba9
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-03 02:30:31 +0500

    add uniqueness validation to viewer model for topic scoped to user

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,4 +1,5 @@
 class Viewer < ApplicationRecord
   belongs_to :topic
   belongs_to :user
+  validates_uniqueness_of :topic_id, scope: :user_id, message: 'already shared with this user.'
 end
--- spec/models/viewer_spec.rb
+++ spec/models/viewer_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Viewer, type: :model do
   it { should belong_to(:topic) }
   it { should belong_to(:user) }
   it { should have_db_index([:topic_id, :user_id]).unique }
+  it do
+    user = create(:user)
+    topic = create(:topic)
+    viewer = create(:viewer, topic: topic, user: user)
+    expect(viewer).to(validate_uniqueness_of(:topic_id)
+      .scoped_to(:user_id)
+      .with_message('already shared with this user.'))
+  end
 end
