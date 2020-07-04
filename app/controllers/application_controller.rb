caa98ce77f93e4408f6bebf47d9a966bce7430dc
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-04 18:54:18 +0500

    update authorization error to be a redirect with message and also remove 403.html, its better to redirect instead of sending a 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   
   rescue_from Pundit::NotAuthorizedError do
-    head :forbidden
+    redirect_to(root_path,
+      alert: 'You are not authorized to access this page.')
   end
   
   before_action :configure_permitted_parameters, if: :devise_controller?
--- spec/requests/topics_request_spec.rb
+++ spec/requests/topics_request_spec.rb
@@ -35,40 +35,4 @@ RSpec.describe "Topics", type: :request do
       expect(subject).to redirect_to(the_origin)
     end
   end
-
-  describe 'edit/update' do
-    let(:random_user) {create(:user)} 
-    let(:edit_path) {edit_topic_path(topic.id, topic.slug)}
-    let(:update_path) {topic_path(topic.id)}
-    
-    context 'topic creator' do
-      before do
-        sign_in topic.creator
-      end
-      it 'permit edit' do
-        get edit_path
-        expect(response).to be_successful
-      end
-
-      it 'permit update' do
-        patch update_path, params: { topic: {title: 'updating the post'} } 
-        follow_redirect!
-        expect(response).to be_successful
-      end
-    end
-
-    context 'random user' do
-      before do
-        sign_in random_user
-      end
-      it 'forbid edit' do
-        get edit_path
-        expect(response).to be_forbidden
-      end
-      it 'forbid update' do
-        patch update_path, params: { topic: {title: 'updating the post'} } 
-        expect(response).to be_forbidden
-      end
-    end
-  end
 end
