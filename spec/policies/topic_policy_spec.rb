a48bc641e4e406523c5262846f1462a31b71bc98
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-05 12:16:01 +0500

    wrote extensive policy tests for creator, simple user and private user for Topic

--- spec/policies/topic_policy_spec.rb
+++ spec/policies/topic_policy_spec.rb
@@ -2,21 +2,55 @@ require 'rails_helper'
 
 RSpec.describe TopicPolicy, type: :policy do
   let(:topic) { create(:topic) }
+  let(:common_actions) { %i[show vote comment] }
 
   context 'topic creator' do
     subject { described_class.new(topic.creator, topic) }
-    it { should permit_edit_and_update_actions}
-    it { should permit_actions(%i[vote sharing])}
+    it { should permit_edit_and_update_actions }
+    it { should permit_actions common_actions.push(:sharing) }
   end
 
   context 'another user' do
     subject { described_class.new(create(:user), topic) }
-    it { should forbid_edit_and_update_actions}
-    it { should forbid_actions(%i[vote sharing])}
+    
+    it { should forbid_edit_and_update_actions }
+    it { should forbid_action(:sharing) }
+    it { should permit_actions(common_actions) }
+  end
 
-    context 'private topic' do
-      it { should forbid_action(:vote)}
+  context 'private topic' do      
+    context 'simple user' do
+      subject do 
+        described_class.new(create(:user), topic.update(visibility: :private))
+      end
+
+      it { should forbid_actions(common_actions)}
+    end
+
+    context 'user with whom topic is shared' do
+      subject do
+        private_user = create(:user)
+        topic = create(:topic, visibility: :private)
+        topic.viewers.create(user: private_user)
+        
+        described_class.new(private_user, topic)
+      end
+
+      it { should permit_actions(common_actions)}
     end
   end
 
 end
+
+# show, vote
+
+# public
+# every user can show
+# every user can vote
+# every user can comment
+
+# private
+# creator can do everything
+# only private_viewers can view
+# only private_viewers can comment
+# only private_viewers can vote
