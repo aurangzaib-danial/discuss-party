781ea69be6fd0d778a7eb256f835802c557cd6e3
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-07 19:09:09 +0500

    adjusted pagination for smaller devices

--- app/helpers/kaminari_fix_helper.rb
+++ app/helpers/kaminari_fix_helper.rb
@@ -3,7 +3,7 @@ module KaminariFixHelper
   # because newer version also loads the items from database
   # also the multiple count statements are run for older version
   def page_entries_info(collection, options = {})
-    entry_name = options[:entry_name] || collection.entry_name.downcase
+    entry_name = options[:entry_name] || collection.entry_name.singularize.downcase
     entry_name = entry_name.pluralize unless collection.total_count == 1
 
       if collection.total_pages < 2
@@ -13,5 +13,9 @@ module KaminariFixHelper
         last = collection.last_page? ? collection.total_count : collection.offset_value + collection.limit_value
         t('helpers.page_entries_info.more_pages.display_entries', :entry_name => entry_name, :first => first, :last => last, :total => collection.total_count)
       end.html_safe
+  end
+
+  def arrow_span(html_code)
+    content_tag :span, html_code.html_safe, 'aria-hidden': true
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -42,10 +42,9 @@ avi_topic.private_viewers << sunny
 
 faker_topic('Book', random_user, :public, tags.second, tags.third, tags.last)
 
-
 some_users = [FactoryBot.create(:user), FactoryBot.create(:user), FactoryBot.create(:user)]
 
-50.times do
+100.times do
   Topic.new.tap do |t|
     t.title = Faker::Book.title
     t.description = Faker::Lorem.paragraph(sentence_count: 15)
