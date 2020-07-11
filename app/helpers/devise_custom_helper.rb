843e845f68f5d48140956b79ea0840ad8421d9f4
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-11 13:08:01 +0500

    add omniauth sign in buttons with styles on sign in and sign up views

--- app/helpers/devise_custom_helper.rb
+++ app/helpers/devise_custom_helper.rb
@@ -2,4 +2,21 @@ module DeviseCustomHelper
   def custom_devise_link(text, path)
     link_to text, path, class: 'btn btn-lg btn-primary btn-block'
   end
+
+  def social_link(provider)
+    site = provider
+    site = site.gsub('_oauth2', '') if site == 'google_oauth2'
+
+    url = send("user_#{provider}_omniauth_authorize_path")
+
+    link_text = "Sign in with #{site.capitalize}"
+
+    link_class = "btn btn-lg btn-block btn-social btn-#{site}"
+
+    span_class = "fab fa-#{site}"
+
+    link_to(url, class: link_class) do
+      content_tag(:span, nil, class: span_class) << link_text
+    end
+  end
 end
